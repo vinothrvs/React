7eeab029b5423b0c0a77c718d1a70987e79fda32
vinoth <vinoth@aivariz.com>
2026-03-24 13:46:40 +0530

    "modified frontend"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello, world!</h1>
+        <marquee><img src={logo} className="App-logo" alt="logo" /></marquee>
+        
+        <marquee><h1>Hello, world!</h1></marquee>
       </header>
     </div>
   );
